3448797e503b644bf532c00f7fd0d1d0b120ed4a
Lesnichenko Stanislav <lesnichenko_stas@mail.ru>
2022-03-31 00:28:49 +0300

    task 3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -2,13 +2,26 @@ function renderCartItem(item) {
     const product = item;
 
     // Задание №3.1. Формирование строки корзины
-
+    
     // product - объект вида {id: id, title: title, count: count, price: price}
-    // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
+    // например, {id: 1, title: 'Пицца', count: 5, price: `500.00`}, где
     // id - идентификатор
     // title - наименование
     // price - цена
     // count - количество
+    item = {id: 1, title: 'Пицца', count: `5`, price: `500` };
+    //product.id = 1;
+    //product.title = 'Пицца';
+    //product.count = 5;
+    //product.price = 500;
+    let itemCountText = '';
+    itemCountText = itemCountText + product.count;
+    itemCountText = itemCountText + ` × `;
+    itemCountText = itemCountText + product.price;
+    itemCountText = itemCountText + ` ₽ = `;
+    let sum = product.price * product.count;
+    itemCountText = itemCountText + sum;
+    itemCountText = itemCountText + ` ₽`;
 
     // сформируйте строку itemCountText вида:
     // количество × цена ₽ = стоимость ₽
