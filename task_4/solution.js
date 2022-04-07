8f827d2ce91374d0ab7cd20f69e5cdb815299c4e
Lesnichenko Stanislav <lesnichenko_stas@mail.ru>
2022-04-07 23:49:23 +0300

    Task 4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,6 +1,6 @@
 // Задание 4.1. Вывод карточек товаров
 
-function parseProducts(JSON) {
+function parseProducts(json) {
     // Аргументом функции является JSON
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
@@ -13,9 +13,9 @@ function parseProducts(JSON) {
 
 function renderProductsCards(json) {
     clearProducts();
-    products = parseProducts(JSON);
+    products = parseProducts(json);
     let length = products.length;
-    for (let i = 0 ; i < length; i +=1);{
+    for (let i = 0; i < length; i +=1) {
         addProduct(products[i]);
     }
     
